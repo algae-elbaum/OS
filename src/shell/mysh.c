8485d13bd86a8be26d8e88d0ca2f5f555dfb97cb
Henry Elbaum <helbaum@caltech.edu>
2016-01-13 08:02:40 +0000

    Still need to parse the commands into the parsed_commands struct and finish off the stuff the comments mention in exec_commands_list Also testing all of it. That's important. Oops. Did implementation in the wrong order or something so the stuff I wrote isn't testable yet

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -1,14 +1,55 @@
+#include <stdlib.h>
+#include <errno.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 
 //Constants
-#define STDIN_FILENO  0
-#define STDOUT_FILENO 1
-#define STDERR_FILENO 2
-#define MAX_LEN 1024
+#define MAX_CMD_SIZE 1024
+// Since doing this in a function can mess up errno maybe
+#define REPORT_ERR(error) {int errsv = errno; \
+                           fprintf(stderr, error); \
+                           fprintf(stderr, ", errno=%d", errsv);};
 
-char* get_prompt()
+typedef struct
+{
+    char *binary;
+    char **args;
+} command;
+
+typedef struct
+{
+    char *file_in;
+    char *file_out;
+    command *commands;
+    int num_commands;
+} parsed_commands;
+
+char *get_prompt();
+void get_commands(char *command_buffer);
+parsed_commands *parse_commands(char *command_buffer);
+int build_pipes(int num_commands, int (*pipes)[2]);
+void free_parsed_commands(parsed_commands *cmds);
+void exec_command(command cmnd);
+void exec_commands_list(parsed_commands *cmds, int (*pipes)[2]);
+
+
+int main(int argc, char const *argv[])
+{
+    char command_buffer[MAX_CMD_SIZE];
+
+    while(1)
+    {
+        get_commands(command_buffer);
+        parsed_commands *cmds = parse_commands(command_buffer);
+        int pipes[cmds->num_commands][2];
+        if (build_pipes(cmds->num_commands, pipes) != -1)
+            exec_commands_list(cmds, pipes);  
+        free_parsed_commands(cmds);
+    }
+}
+
+char *get_prompt()
 {
     /*
     Generates a command prompt.
@@ -23,19 +64,126 @@ char* get_prompt()
     prompt = strcat(prompt, directory);
     prompt = strcat(prompt, ">>>");
     return prompt;
-} 
+}
 
-
-
-
-int main(int argc, char const *argv[])
+// Pound characters from stdin into the command buffer until a newline comes in
+// As per assignment, assuming all commands are < 1 KiB
+void get_commands(char *command_buffer)
 {
-    char input[MAX_LEN];
-    while (1)
+    printf("%s\n", get_prompt());
+    fgets(command_buffer, MAX_CMD_SIZE, stdin);
+}
+
+// Build a parsed commands struct from the command in the command buffer
+parsed_commands *parse_commands(char *command_buffer)
+{
+    //TODO
+    return NULL;
+}
+
+// Make all the pipes
+int build_pipes(int num_commands, int (*pipes)[2])
+{
+    int i;
+    for (i = 0; i < num_commands - 1; i++)
     {
-        printf("%s\n", get_prompt());
-        fgets(input, MAX_LEN, stdin);
-        printf("You typed: %s\n", input);
+        if (pipe(pipes[i]) == -1)
+        {
+            REPORT_ERR("Couldn't open pipe");
+            return -1; // Failure
+        }
     }
-    return 0;
+    return 0; // Success
+}
+
+// Does what it says
+void free_parsed_commands(parsed_commands *cmds)
+{
+    //TODO
+}
+
+// Here's the hard work. Spawn all the child processes, piping and redirecting 
+// appropriately. Then wait for them to finish and return
+void exec_commands_list(parsed_commands *cmds, int (*pipes)[2])
+{
+    // Special cases:
+    // In these special cases 'binary' is a misnomer, but otherwise in general
+    // it makes it more clear what's going on
+    if (strcmp(cmds->commands[0].binary, "cd") == 0 
+            || strcmp(cmds->commands[0].binary, "chdir") == 0)
+    {
+        // This assumes it's just given one argument. May want to add more 
+        // error checking
+        if (chdir(cmds->commands[0].args[0]) == -1)
+        {
+            REPORT_ERR("Couldn't change directory");
+        }
+        return;
+    }
+    if (strcmp(cmds->commands[0].binary, "exit") == -1)
+    {
+        // This one's easy!
+        free_parsed_commands(cmds);
+        exit(0);
+    }
+    if (strcmp(cmds->commands[0].binary, "make_me_a_sandwich") == 0)
+    {
+        // ascii from http://www.ascii-art.de/ascii/s/sandwich.txt
+        printf("                    _.---._\
+                _.-~       ~-._\
+            _.-~               ~-._\
+        _.-~                       ~---._\
+    _.-~                                 ~\\\
+ .-~                                    _.;\
+ :-._                               _.-~ ./\
+ }-._~-._                   _..__.-~ _.-~)\
+ `-._~-._~-._              / .__..--~_.-~\
+     ~-._~-._\\.        _.-~_/ _..--~~\
+         ~-. \\`--...--~_.-~/~~\
+            \\.`--...--~_.-~\
+              ~-..----~");
+        return;
+    }
+
+    // else it's a list of binaries to pipe together, along with the redirects
+    // TODO the rest of this
+    /* 
+       fork each child from the parent, pipe them, redirect them
+     */
+    int child_pids[cmds->num_commands];
+    int i;
+    for (i = 0; i < cmds->num_commands; i++)
+    {
+        int child_pid = fork();
+        if (child_pid == -1)
+        {
+            REPORT_ERR("Fork failed");
+            //TODO ohshit what do we do... do children wait to run until they
+            // know somehow that all forks succeeded?
+        }
+        if (child_pid == 0) // am child
+        {
+            // Set up redirects
+            if (i == 0) // am first child
+            {
+//                dup2(, STDIN_FILENO);
+            }
+            if (i == cmds->num_commands - 1) // am last child
+            {
+//                dup2(, STDOUT_FILENO);
+            }
+            
+            // Set up pipes
+//            dup2(, STDIN_FILENO);
+//            dup2(, STDOUT_FILENO);
+
+            // And run
+//            execlp();
+              // children should never ever get here
+        }
+        // else am still command line
+        child_pids[i] = child_pid;
+    }
+    // Only the command line gets here
+    // Wait for all the children to finish, then return
 }
