69f6381c8eb54eab86a171fda1cfb2fb68ed897e
helbaum <helbaum@caltech.edu>
2016-02-17 18:48:13 -0800

    Added a whole bunch of the file syscalls (they're mostly wrappers) and a lock to protect against concurrent filesystem calls Changed to associate file descriptors to be process specific instead of global.
    Warning: most things (everything maybe) won't work until we get proper dereferencing of user space pointers

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,18 +3,29 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/synch.h"
 #include "filesys/filesys.h"
 #include "filesys/file.h"
+#include "devices/input.h"
 
-#define MAX_FILES 256 // Arbitrary limit on number of files
 
 static void syscall_handler(struct intr_frame *);
 
+static int sys_filesize (int fd);
+static int sys_read(int fd, void *buffer, unsigned size);
+static int sys_write(int fd, const void *buffer, unsigned size);
+static void sys_seek(int fd, unsigned pos);
+static unsigned sys_tell(int fd);
+static void sys_close(int fd);
+
+static struct lock filesys_lock;
+
+
 void syscall_init(void) {
+    lock_init(&filesys_lock);
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static int sys_write(int fd, const void *buffer, unsigned size);
 
 static void syscall_handler(struct intr_frame *f UNUSED) {
     long intr_num = *((long *) f->esp) ;
@@ -33,41 +44,52 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
         case  SYS_WAIT:                   /*!< Wait for a child process to die. */
             break;
         case  SYS_CREATE:                 /*!< Create a file. */
+            lock_acquire(&filesys_lock);
+            
+
+            lock_release(&filesys_lock);
             break;
         case  SYS_REMOVE:                 /*!< Delete a file. */
+            lock_acquire(&filesys_lock);
+            
+            
+            lock_release(&filesys_lock);
             break;
         case  SYS_OPEN:                   /*!< Open a file. */
+            lock_acquire(&filesys_lock);
+            
+            
+            lock_release(&filesys_lock);
             break;
         case  SYS_FILESIZE:               /*!< Obtain a file's size. */
+            lock_acquire(&filesys_lock);
+            f->eax = sys_filesize(arg0);
+            lock_release(&filesys_lock);
             break;
         case  SYS_READ:                   /*!< Read from a file. */
+            lock_acquire(&filesys_lock);
+            f->eax = sys_read(arg0, (void *)arg1, arg2);
+            lock_release(&filesys_lock);
             break;
         case  SYS_WRITE:                  /*!< Write to a file. */
+            lock_acquire(&filesys_lock);
             f->eax = sys_write(arg0, (void *)arg1, arg2);
+            lock_release(&filesys_lock);
             break;
         case  SYS_SEEK:                   /*!< Change position in a file. */
+            lock_acquire(&filesys_lock);
+            sys_seek(arg0, arg1);
+            lock_release(&filesys_lock);
             break;
         case  SYS_TELL:                   /*!< Report current position in a file. */
+            lock_acquire(&filesys_lock);
+            f->eax = sys_tell(arg0);
+            lock_release(&filesys_lock);
             break;
         case  SYS_CLOSE:                  /*!< Close a file. */
-            break;
-
-        /* Project 3 and optionally project 4. */
-        case  SYS_MMAP:                   /*!< Map a file into memory. */
-            break;
-        case  SYS_MUNMAP:                 /*!< Remove a memory mapping. */
-            break;
-
-        /* Project 4 only. */
-        case  SYS_CHDIR:                  /*!< Change the current directory. */
-            break;
-        case  SYS_MKDIR:                  /*!< Create a directory. */
-            break;
-        case  SYS_READDIR:                /*!< Reads a directory entry. */
-            break;
-        case  SYS_ISDIR:                  /*!< Tests if a fd represents a directory. */
-            break;
-        case  SYS_INUMBER:                 /*!< Returns the inode number for a fd. */
+            lock_acquire(&filesys_lock);
+            sys_close(arg0);
+            lock_release(&filesys_lock);
             break;
         default:
             printf("system call!\n");
@@ -78,36 +100,82 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
 
 ////// File syscalls \\\\\\\/
 
-// I'd like to identify file descriptors with processes somehow, but it doesn't seem like the 
-// intr_frame holds anything uniquely identifying. Eventually should use thread_current for some
-// sort of map, but for now I just want to get this working
-// So instead a user process could perfectly well use a file descriptor that wasn't given to it
-static struct file *open_files[MAX_FILES];
-static int used_fds[MAX_FILES] = {0};
 
-// Gets the first free file descriptor. If none are available, returns -1
+// This'll be useful for opening a new file
 static int find_available_fd(void)
 {
     int i;
     for (i = 0; i < MAX_FILES; i++)
     {
-        if (! used_fds[i])
+        if (thread_current()->open_files[i] != NULL)
             return i;
     }
     return -1;
 }
 
+static bool check_fd(int fd)
+{
+    return fd >= 0 && fd < MAX_FILES && (thread_current()->open_files[fd] != NULL);
+}
+
+static int sys_filesize(int fd)
+{
+    if (check_fd(fd))
+        return file_length(thread_current()->open_files[fd]); 
+
+    thread_exit(); // File doesn't exist. Let's destroy this process
+}
+
+static int sys_read(int fd, void *buffer, unsigned size)
+{
+    if (fd == 0)
+    {
+        unsigned i;
+        for (i = 0; i < size; i++)
+            ((char *)buffer)[i] = input_getc();
+        return size;
+    }
+    if (check_fd(fd))
+        return file_read(thread_current()->open_files[fd], buffer, size);
+
+    thread_exit(); // File doesn't exist. Let's destroy this process
+}
 
 static int sys_write(int fd, const void *buffer, unsigned size)
 {
-//    printf("%d", fd);
-    return 1;
     if (fd == 1)
     {
         putbuf(buffer, size);
         return size;
     }
-    if (used_fds[fd])
-        return file_write (open_files[fd], buffer, size);
-    return -1; // Or maybe call sys_exit for murder fun time
+    if (check_fd(fd))
+        return file_write(thread_current()->open_files[fd], buffer, size);
+
+    thread_exit(); // File doesn't exist. Let's destroy this process
 }
+
+static void sys_seek(int fd, unsigned pos)
+{
+    if (check_fd(fd))
+        file_seek(thread_current()->open_files[fd], pos); 
+    else
+        thread_exit(); // File doesn't exist. Let's destroy this process
+}
+
+static unsigned sys_tell(int fd)
+{
+    if (check_fd(fd))
+        return file_tell(thread_current()->open_files[fd]); 
+
+    thread_exit(); // File doesn't exist. Let's destroy this process
+}
+
+static void sys_close(int fd)
+{
+    if (check_fd(fd))
+        file_close(thread_current()->open_files[fd]); 
+    else
+        thread_exit(); // File doesn't exist. Let's destroy this process
+}
+
+
