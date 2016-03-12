dcb7f0a36b8caa2e76e3d64cc7040cfe08bdfa4e
helbaum <helbaum@caltech.edu>
2016-03-11 16:02:10 -0800

    Changing syscall.c back to the better working version, lowering MAX_FILES to something more reasonable

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -10,7 +10,7 @@
 #include <list.h>
 #include <stdint.h>
 
-#define MAX_FILES 256 // Arbitrary limit on number of open files
+#define MAX_FILES 16 // Arbitrary limit on number of open files
 
 /*! States in a thread's life cycle. */
 enum thread_status {
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,45 +1,47 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <string.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/synch.h"
 #include "filesys/filesys.h"
 #include "filesys/file.h"
 #include "userprog/process.h"
-
-#define MAX_FILES 256 // Arbitrary limit on number of files
+#include "lib/user/syscall.h"
+#include "threads/malloc.h"
 #include "devices/shutdown.h"
 #include "devices/input.h"
 //#include "threads/pte.h"
 #include "userprog/pagedir.h"
 #include "threads/vaddr.h"
 
-static void syscall_handler(struct intr_frame *);
+struct lock filesys_lock;
 
-static int sys_filesize (int fd);
-static int sys_read(int fd, void *buffer, unsigned size);
-static int sys_write(int fd, const void *buffer, unsigned size);
-static void sys_seek(int fd, unsigned pos);
-static unsigned sys_tell(int fd);
-static void sys_close(int fd);
-static int find_available_fd(void);
+static void syscall_handler(struct intr_frame *);
 
 static void syscall_exit(int status);
 
-static struct lock filesys_lock;
-
-static void * ptr_is_valid(const void *ptr);
+static void * check_and_convert_ptr(const void *ptr);
 
 static bool is_white_space(char input)
 {
    return (input == '\0' || input == '\r' || input == '\n');
 }
 
+static bool is_valid_filename(const char *file)
+{
+    return file != NULL
+        && file[0] != '\0'
+        && strlen(file) < 15
+        && strlen(file) > 0
+        && !is_white_space(file[0]);
+}
+
 // check whether a passed in pointer is valid, return correct address
 // currently doing it the slower way, will attempt to implemenent 2nd way
 // if time remains
-static void * ptr_is_valid(const void *ptr)
+static void * check_and_convert_ptr(const void *ptr)
 {   // check whether the ptr address is valid and within user virtual memory   
     // page directory is most significant 10 digits of vaddr
     // mimicking pd_no. PDSHIFT = (PTSHIFT + PTBITS) = PGBITS + 10 = 22
@@ -50,16 +52,14 @@ static void * ptr_is_valid(const void *ptr)
         //pagedir_destroy((uint32_t *) pd);
         syscall_exit(-1);
     }
-    else // if the location is valid
-    {
-        // lookup page uses page directory (most significant 10 digits of
-        // vaddr), vaddr, and CREATE = true(if not found, 
-        // create new page table, return its ptr) or false(if not found,
-        // return null)
-        return pagedir_get_page(thread_current()->pagedir, ptr);
-    }
+    // if the location is valid:
+    // lookup page uses page directory (most significant 10 digits of
+    // vaddr), vaddr, and CREATE = true(if not found, 
+    // create new page table, return its ptr) or false(if not found,
+    // return null)
+    return pagedir_get_page(thread_current()->pagedir, ptr);
 }
-    
+
 void syscall_init(void) {
     lock_init(&filesys_lock);
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
@@ -79,9 +79,9 @@ static void syscall_exit(int status)
 }
 static void syscall_exec(char * name)
 {
-    if (ptr_is_valid((void*) name) != NULL)
+    if (is_valid_filename(name))
     {
-        process_execute((char*)ptr_is_valid((void*) name));
+        process_execute(name);
     }
     else
     {
@@ -91,24 +91,22 @@ static void syscall_exec(char * name)
 static bool syscall_create(const char * file, unsigned size)
 {
     //maybe we need something else here?
-    if (ptr_is_valid((void*) file) != NULL)
+    if (is_valid_filename(file))
     {
-        return filesys_create((char*)ptr_is_valid((void*) file), size);
+        return filesys_create(file, size);
     }
-    else
-    {
+    // Special cases
+    if (file == NULL || file[0] == '\0')
         syscall_exit(-1);
-    }
-    return 0;
+    return false;
 }
 
 static bool syscall_remove(const char *file)
 {
     // gotta do some other check maybe???
-    char * f = (char *) ptr_is_valid((void*) file);
-    if (! is_white_space(*f))
+    if (is_valid_filename(file))
     {
-        return filesys_remove(f);
+        return filesys_remove(file);
     }
     else
     {
@@ -116,42 +114,137 @@ static bool syscall_remove(const char *file)
     }
     return 0;
 }
+
+// This'll be useful for opening a new file
+static int find_available_fd(void)
+{
+    int i;
+    struct thread *t = thread_current();
+    for (i = 2; i < MAX_FILES; i++)
+    {
+        if (t->open_files[i] == NULL)
+            return i;
+    }
+    return -1;
+}
+
+static bool check_fd(int fd)
+{
+    return fd >= 0 && fd < MAX_FILES && (thread_current()->open_files[fd] != NULL);
+}
+
 static int syscall_open(const char *file)
 {
-    char * f = (char *) ptr_is_valid((void*) file);
-    if (! is_white_space(*f))
+    int fd;
+    if (is_valid_filename(file) && (fd = find_available_fd()) != -1)
     {
-        int fd = find_available_fd();
-        thread_current()->open_files[fd] = filesys_open(f);
-        if (thread_current()->open_files[fd] == NULL)
+        struct thread *t = thread_current();
+        t->open_files[fd] = filesys_open(file);
+        if (t->open_files[fd] == NULL)
         {
             return -1;
         }
         // deny writes to executables
-        
+
         return fd;
     }
-    else
-    {
-        syscall_exit(-1);
-    }
-    return 0;
+    // Seemingly arbitrary different ways of dealing with different bad input
+    if (file != NULL && file[0] == '\0')
+        return -1;
+    syscall_exit(-1);
+    return -1;
 }
+
+
+static int syscall_filesize(int fd)
+{
+    if (check_fd(fd))
+        return file_length(thread_current()->open_files[fd]); 
+    syscall_exit(-1); // File doesn't exist. Let's destroy this process
+    return -1; // Keep the compiler happy
+}
+
+static int syscall_read(int fd, void *buffer, unsigned size)
+{
+    if (fd == 0)
+    {
+        unsigned i;
+        for (i = 0; i < size; i++)
+            ((char *)buffer)[i] = input_getc();
+        return size;
+    }
+    if (check_fd(fd) && buffer != NULL)
+        return file_read(thread_current()->open_files[fd], buffer, size);
+
+    syscall_exit(-1); // File doesn't exist. Let's destroy this process
+    return -1; // Keep the compiler happy 
+}
+
+static int syscall_write(int fd, const void *buffer, unsigned size)
+{
+    if (fd == 1)
+    {
+        putbuf(buffer, size);
+        return size;
+    }
+    if (check_fd(fd) && buffer != NULL)
+        return file_write(thread_current()->open_files[fd], buffer, size);
+
+    syscall_exit(-1); // File doesn't exist. Let's destroy this process
+    return -1; // Keep the compiler happy
+}
+
+static void syscall_seek(int fd, unsigned pos)
+{
+    if (check_fd(fd))
+        file_seek(thread_current()->open_files[fd], pos); 
+    else
+        syscall_exit(-1); // File doesn't exist. Let's destroy this process
+}
+
+static unsigned syscall_tell(int fd)
+{
+    if (check_fd(fd))
+        return file_tell(thread_current()->open_files[fd]); 
+
+    syscall_exit(-1); // File doesn't exist. Let's destroy this process
+    return -1; // Keep the compiler happy
+}
+
+static void syscall_close(int fd)
+{
+    if (check_fd(fd))
+    {
+        struct thread *t = thread_current();
+        file_close(t->open_files[fd]); 
+        t->open_files[fd] = NULL;
+    }
+    else
+        syscall_exit(-1); // File doesn't exist. Let's destroy this process
+}
+
 static int syscall_wait(int pid)
 {
     return process_wait(pid);
 }
 
-static void syscall_handler(struct intr_frame *f UNUSED) {
+struct map
+{
+    mapid_t id;
+    struct list_elem elem;
+    struct list pages;
+};
+
+static void syscall_handler(struct intr_frame *f) {
 
     long intr_num;
     long *arg0;
     long *arg1;
     long *arg2;
 
-    if (ptr_is_valid((void*) f->esp) != NULL)
+    if (check_and_convert_ptr((void*) f->esp) != NULL)
     {
-        intr_num = *((long *) f->esp) ;
+        intr_num = *((long *) f->esp);
         arg0 = (((long *) f->esp) + 1); 
         arg1 = (((long *) f->esp) + 2); 
         arg2 = (((long *) f->esp) + 3); 
@@ -160,7 +253,12 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
     {
         syscall_exit(-1);
     }
-    
+    // The tests imply this is necessary even when none of the arguments are needed
+    if (!is_user_vaddr(arg0))
+    {
+        syscall_exit(-1);
+    }
+
     switch (intr_num)
     {
         /* Projects 2 and later. */
@@ -169,156 +267,67 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
             syscall_halt();
             break;
         case  SYS_EXIT:                   /*!< Terminate this process. */
-            if(ptr_is_valid(arg0))
-            {
-                syscall_exit(*arg0);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
+            syscall_exit(*arg0);
             break;
         case  SYS_EXEC:                   /*!< Start another process. */
-            if(ptr_is_valid(arg0))
-            {
-		syscall_exec(*(char **) arg0);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
+            syscall_exec((char *) check_and_convert_ptr((char *) *arg0));
             break;
         case  SYS_WAIT:                   /*!< Wait for a child process to die. */
             // process wait
-            if(ptr_is_valid(arg0))
-            {
-	        syscall_wait((int) *arg0);
-            }
-            else
-            {
-                syscall_exit(-1);
-            } 
+            syscall_wait((int) *arg0);
+            break;
         case  SYS_CREATE:                 /*!< Create a file. */
             lock_acquire(&filesys_lock);
-            if(ptr_is_valid(arg0) && ptr_is_valid(arg1))
-            {
-                syscall_create(*(char **) arg0, (int) *arg1);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
+            f->eax = syscall_create((char *) check_and_convert_ptr((char *) *arg0), *arg1);
             lock_release(&filesys_lock);
             break;
         case  SYS_REMOVE:                 /*!< Delete a file. */
             lock_acquire(&filesys_lock);
-            if(ptr_is_valid(arg0))
-            {
-		syscall_remove(*(char **) arg0);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
+            f->eax = syscall_remove((char *) check_and_convert_ptr((char *) *arg0));
             lock_release(&filesys_lock);
             break;
         case  SYS_OPEN:                   /*!< Open a file. */
             lock_acquire(&filesys_lock);
-            if(ptr_is_valid(arg0))
-            {
-                syscall_open(*(char **) arg0);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
-
+            f->eax = syscall_open((char *) check_and_convert_ptr((char *) *arg0));
             lock_release(&filesys_lock);
             break;
         case  SYS_FILESIZE:               /*!< Obtain a file's size. */
             lock_acquire(&filesys_lock);
-            if(ptr_is_valid(arg0))
-            {
-                f->eax = sys_filesize(*arg0);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
+            f->eax = syscall_filesize(*arg0);
             lock_release(&filesys_lock);
             break;
         case  SYS_READ:                   /*!< Read from a file. */
             lock_acquire(&filesys_lock);
-            if(ptr_is_valid(arg0) && ptr_is_valid(arg1) && ptr_is_valid(arg2))
-            {
-                f->eax = sys_read(*arg0, *(void **)arg1, *arg2);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
+            f->eax = syscall_read(*arg0, check_and_convert_ptr((void *) *arg1), *arg2);
             lock_release(&filesys_lock);
             break;
         case  SYS_WRITE:                  /*!< Write to a file. */
             lock_acquire(&filesys_lock);
-            if(ptr_is_valid(arg0) && ptr_is_valid(arg1) && ptr_is_valid(arg2))
-            {
-                f->eax = sys_write(*arg0,* (void **)arg1, *arg2);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
+            f->eax = syscall_write(*arg0, check_and_convert_ptr((void *) *arg1), *arg2);
             lock_release(&filesys_lock);
             break;
         case  SYS_SEEK:                   /*!< Change position in a file. */
             lock_acquire(&filesys_lock);
-            if(ptr_is_valid(arg0) && ptr_is_valid(arg1))
-            {
-		sys_seek(*arg0, *arg1);                
-            }
-            else
-            {
-                syscall_exit(-1);
-            }           
+            syscall_seek(*arg0, *arg1);
             lock_release(&filesys_lock);
             break;
         case  SYS_TELL:                   /*!< Report current position in a file. */
             lock_acquire(&filesys_lock);
-            if(ptr_is_valid(arg0))
-            {
-                f->eax = sys_tell(*arg0);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
+            f->eax = syscall_tell(*arg0);
             lock_release(&filesys_lock);
             break;
         case  SYS_CLOSE:                  /*!< Close a file. */
-
             lock_acquire(&filesys_lock);
-            if(ptr_is_valid(arg0))
-            {
-                sys_close(*arg0);
-            }
-            else
-            {
-                syscall_exit(-1);
-            }
-
+            syscall_close(*arg0);
             lock_release(&filesys_lock);
             break;
-
-        // These are for later projects. 
-
-        /* Project 3 and optionally project 4. */
         // case  SYS_MMAP:                   /*!< Map a file into memory. */
+        //     f->eax = syscall_mmap(*arg0, check_and_convert_ptr((void *) *arg1));
         //     break;
         // case  SYS_MUNMAP:                 /*!< Remove a memory mapping. */
+        //     syscall_munmap(*arg0);
         //     break;
 
-        // /* Project 4 only. */
         // case  SYS_CHDIR:                  /*!< Change the current directory. */
         //     break;
         // case  SYS_MKDIR:                  /* !< Create a directory. */
@@ -332,82 +341,4 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
         default:
             syscall_exit(-1);
     }
-}
-
-
-// This'll be useful for opening a new file
-static int find_available_fd(void)
-{
-    int i;
-    for (i = 2; i < MAX_FILES; i++)
-    {
-        if (thread_current()->open_files[i] != NULL)
-            return i;
-    }
-    return -1;
-}
-
-static bool check_fd(int fd)
-{
-    return fd >= 0 && fd < MAX_FILES && (thread_current()->open_files[fd] != NULL);
-}
-
-static int sys_filesize(int fd)
-{
-    if (check_fd(fd))
-        return file_length(thread_current()->open_files[fd]); 
-
-    thread_exit(); // File doesn't exist. Let's destroy this process
-}
-
-static int sys_read(int fd, void *buffer, unsigned size)
-{
-    if (fd == 0)
-    {
-        unsigned i;
-        for (i = 0; i < size; i++)
-            ((char *)buffer)[i] = input_getc();
-        return size;
-    }
-    if (check_fd(fd))
-        return file_read(thread_current()->open_files[fd], buffer, size);
-
-    thread_exit(); // File doesn't exist. Let's destroy this process
-}
-
-static int sys_write(int fd, const void *buffer, unsigned size)
-{
-    if (fd == 1)
-    {
-        putbuf(buffer, size);
-        return size;
-    }
-    if (check_fd(fd))
-        return file_write(thread_current()->open_files[fd], buffer, size);
-
-    thread_exit(); // File doesn't exist. Let's destroy this process
-}
-
-static void sys_seek(int fd, unsigned pos)
-{
-    if (check_fd(fd))
-        file_seek(thread_current()->open_files[fd], pos); 
-    else
-        thread_exit(); // File doesn't exist. Let's destroy this process
-}
-
-static unsigned sys_tell(int fd)
-{
-    if (check_fd(fd))
-        return file_tell(thread_current()->open_files[fd]); 
-
-    thread_exit(); // File doesn't exist. Let's destroy this process
-}
-
-static void sys_close(int fd)
-{
-    if (check_fd(fd))
-        file_close(thread_current()->open_files[fd]); 
-    else
-        thread_exit(); // File doesn't exist. Let's destroy this process
 }
